45cc1c4b9913c997ecacb328fe2a94524370a86f
Josh Black <josh@josh.black>
2024-02-15 13:03:30 -0600

    Remove basePath with new GitHub Pages url

--- website/next.config.js
+++ website/next.config.js
@@ -1,10 +1,7 @@
-const { GITHUB_PAGES } = process.env;
-
 /**
  * @type {import('next').NextConfig}
  */
 const config = {
-  basePath: GITHUB_PAGES === 'true' ? '/design-systems' : '',
   output: 'export',
 };
 
