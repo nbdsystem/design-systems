0cacb774079435a1397409e48dc5c58d4c9612f1
Josh Black <joshblack@github.com>
2024-02-15 12:20:49 -0600

    fix: add basePath for GitHub Pages

--- website/next.config.js
+++ website/next.config.js
@@ -1,7 +1,10 @@
+const { GITHUB_PAGES } = process.env;
+
 /**
  * @type {import('next').NextConfig}
  */
 const config = {
+  basePath: GITHUB_PAGES === 'true' ? '/design-systems' : '',
   output: 'export',
 };
 
